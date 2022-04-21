656337300a84ab156cdec7a400e89cf59a6dbaca
minzhou1900 <55419911+minzhou1900@users.noreply.github.com>
2022-04-21 12:04:55 +0800

    Create dgcnn.py

---
+++ TeleGraph/SEAL/dgcnn.py
@@ -0,0 +1,55 @@
+import math
+import torch
+import torch.nn.functional as F
+from torch.nn import ModuleList, Conv1d, MaxPool1d, Linear
+from torch_geometric.nn import GCNConv, global_sort_pool
+
+
+class DGCNN(torch.nn.Module):
+    def __init__(self, train_dataset, hidden_channels, num_layers, GNN=GCNConv, k=0.6):
+        super(DGCNN, self).__init__()
+
+        if k < 1:  # Transform percentile to number.
+            num_nodes = sorted([data.num_nodes for data in train_dataset])
+            k = num_nodes[int(math.ceil(k * len(num_nodes))) - 1]
+            k = max(10, k)
+        self.k = int(k)
+
+        self.convs = ModuleList()
+        self.convs.append(GNN(train_dataset[0].num_features, hidden_channels))
+        for i in range(0, num_layers - 1):
+            self.convs.append(GNN(hidden_channels, hidden_channels))
+        self.convs.append(GNN(hidden_channels, 1))
+
+        conv1d_channels = [16, 32]
+        total_latent_dim = hidden_channels * num_layers + 1
+        conv1d_kws = [total_latent_dim, 5]
+        self.conv1 = Conv1d(1, conv1d_channels[0], conv1d_kws[0],
+                            conv1d_kws[0])
+        self.maxpool1d = MaxPool1d(2, 2)
+        self.conv2 = Conv1d(conv1d_channels[0], conv1d_channels[1],
+                            conv1d_kws[1], 1)
+        dense_dim = int((self.k - 2) / 2 + 1)
+        dense_dim = (dense_dim - conv1d_kws[1] + 1) * conv1d_channels[1]
+        self.lin1 = Linear(dense_dim, 128)
+        self.lin2 = Linear(128, 1)
+
+    def forward(self, x, edge_index, batch):
+        xs = [x]
+        for conv in self.convs:
+            xs += [torch.tanh(conv(xs[-1], edge_index))]
+        x = torch.cat(xs[1:], dim=-1)
+
+        # Global pooling.
+        x = global_sort_pool(x, batch, self.k)
+        x = x.unsqueeze(1)  # [num_graphs, 1, k * hidden]
+        x = F.relu(self.conv1(x))
+        x = self.maxpool1d(x)
+        x = F.relu(self.conv2(x))
+        x = x.view(x.size(0), -1)  # [num_graphs, dense_dim]
+
+        # MLP.
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        return x
