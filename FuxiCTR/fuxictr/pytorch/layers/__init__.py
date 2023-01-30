7f33f9b76fb763257e2c3f090aadb2aa8c0d89d6
xpai <73411968+xpai@users.noreply.github.com>
2023-01-30 16:19:56 +0800

    Update FuxiCTR v2 with new benchmark models (#7)
    Co-authored-by: xpai <xpai@users.noreply.github.com>

--- FuxiCTR/fuxictr/pytorch/layers/__init__.py
+++ FuxiCTR/fuxictr/pytorch/layers/__init__.py
@@ -1,19 +1,9 @@
-# Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+from .pooling import *
+from .embeddings import *
+from .activations import *
+from .blocks import *
+from .interactions import *
+from .attentions import *
 
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the MIT license.
-
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE. See the MIT License for more details.
-
-from .embedding import *
-from .activation import *
-from .shallow import *
-from .deep import *
-from .sequence import *
-from .attention import *
-from .interaction import *
-from .graph import *
 
 
--- FuxiCTR/model_zoo/README.md
+++
@@ -1,7 +0,0 @@
-## Model Zoo
-
-This model zoo contains the following list of models:
-
-+ [IJCAI'21] UNBERT: User-News Matching BERT for News Recommendation
-
-
